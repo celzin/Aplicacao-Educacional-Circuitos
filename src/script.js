d8a5fedcde6faa2acacff3a4c15e0f241cc1c640
Pedro Dias <phpdias@outlook.com>
2023-11-05 14:43:32 -0300

    feat [funcoes]: adicionando funcoes para cada circuito que vai ter

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-let selectedImage = null;
+let selectedImage = 1;
 
 function selectImage(imageNumber) {
     selectedImage = imageNumber;
@@ -6,13 +6,66 @@ function selectImage(imageNumber) {
 }
 
 function simulate() {
-    const resistencia = document.getElementById('resistencia').value;
-    const capacitancia = document.getElementById('capacitancia').value;
-    const condicaoInicial = document.getElementById('condicaoInicial').value;
+    const resistencia = parseInt(document.getElementById('resistencia').value, 10);
+    const capacitancia = parseInt(document.getElementById('capacitancia').value, 10);
+    const condicaoInicial = parseInt(document.getElementById('condicaoInicial').value, 10);
+
+    let resultado = 0;
 
     // Aqui você pode adicionar a lógica para calcular os valores de acordo com a imagem selecionada
+    switch (parseInt(selectedImage)){
+        case 1:
+            resultado = calc_c1(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 2:
+            resultado = calc_c2(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 3:
+            resultado = calc_c3(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 4:
+            resultado = calc_c4(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 5:
+            resultado = calc_c5(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 6:
+            resultado = calc_c6(resistencia, capacitancia, condicaoInicial);
+            break;
+        default:
+    }
 
     // Por agora, estou apenas exibindo valores fictícios
     document.getElementById('chart').innerText = `Gráfico do Circuito ${selectedImage}`;
-    document.getElementById('outputValue').innerText = "Valor de Saída (V): 5V";
+    document.getElementById('outputValue').innerText = `Valor de Saída (V): ${resultado}V`;
+}
+
+function calc_c1(resistencia, capacitancia, condicaoInicial){
+    let somaTotal = resistencia + capacitancia + condicaoInicial;
+    return somaTotal;
+}
+
+function calc_c2(resistencia, capacitancia, condicaoInicial){
+    let valorR = resistencia;
+    return valorR;
+}
+
+function calc_c3(resistencia, capacitancia, condicaoInicial){
+    let valorC = capacitancia;
+    return valorC;
+}
+
+function calc_c4(resistencia, capacitancia, condicaoInicial){
+    let valorCI = condicaoInicial;
+    return valorCI;
+}
+
+function calc_c5(resistencia, capacitancia, condicaoInicial){
+    let somaRC = resistencia + capacitancia;
+    return somaRC;
+}
+
+function calc_c6(resistencia, capacitancia, condicaoInicial){
+    let somaRCI = (resistencia + condicaoInicial);
+    return somaRCI;
 }
