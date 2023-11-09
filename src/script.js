2f6fdb85e5ebcf8f90af13dd2a8f562cc6c03061
Pedro Dias <phpdias@outlook.com>
2023-11-09 08:06:33 -0300

    feat [scriptjs]: upando o scriptjs

---
+++ src/script.js
@@ -0,0 +1,78 @@
+let selectedImage = 1;
+
+function selectImage(imageNumber) {
+    selectedImage = imageNumber;
+    console.log(imageNumber);
+}
+
+function simulate() {
+    const resistencia = parseInt(document.getElementById('idresistencia').value, 10);
+    const capacitancia = parseInt(document.getElementById('idcapacitancia').value, 10);
+    const condicaoInicial = parseInt(document.getElementById('idcondicaoInicial').value, 10);
+
+    console.log(resistencia);
+    console.log(capacitancia);
+    console.log(condicaoInicial);
+
+    if (isNaN(resistencia) || isNaN(capacitancia) || isNaN(condicaoInicial)) {
+        alert('Por favor, insira valores numéricos válidos em todos os campos.');
+        return;
+    }
+
+    let resultado = 0;
+
+    switch (parseInt(selectedImage)){
+        case 1:
+            resultado = calc_c1(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 2:
+            resultado = calc_c2(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 3:
+            resultado = calc_c3(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 4:
+            resultado = calc_c4(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 5:
+            resultado = calc_c5(resistencia, capacitancia, condicaoInicial);
+            break;
+        case 6:
+            resultado = calc_c6(resistencia, capacitancia, condicaoInicial);
+            break;
+        default:
+    }
+
+    document.getElementById('chart').innerText = `Gráfico do Circuito ${selectedImage}`;
+    document.getElementById('outputValue').innerText = `Valor de Saída (V): ${resultado}V`;
+}
+
+function calc_c1(resistencia, capacitancia, condicaoInicial){
+    let somaTotal = resistencia + capacitancia + condicaoInicial;
+    return somaTotal;
+}
+
+function calc_c2(resistencia, capacitancia, condicaoInicial){
+    let valorR = resistencia;
+    return valorR;
+}
+
+function calc_c3(resistencia, capacitancia, condicaoInicial){
+    let valorC = capacitancia;
+    return valorC;
+}
+
+function calc_c4(resistencia, capacitancia, condicaoInicial){
+    let valorCI = condicaoInicial;
+    return valorCI;
+}
+
+function calc_c5(resistencia, capacitancia, condicaoInicial){
+    let somaRC = resistencia + capacitancia;
+    return somaRC;
+}
+
+function calc_c6(resistencia, capacitancia, condicaoInicial){
+    let somaRCI = (resistencia + condicaoInicial);
+    return somaRCI;
+}
