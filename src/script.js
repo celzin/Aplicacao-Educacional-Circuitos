539badd2d3ac0799c040343529c328713720c110
celzin <celsovinicius4@gmail.com>
2023-11-25 17:25:17 -0300

    funcionando 3

--- src/script.js
+++ src/script.js
@@ -5,89 +5,6 @@ function selectImage(imageNumber) {
     console.log(imageNumber);
 }
 
-// function simulate() {
-//     const resistencia = parseInt(document.getElementById('idresistencia').value, 10);
-//     const capacitancia = parseInt(document.getElementById('idcapacitancia').value, 10);
-//     const condicaoInicial = parseInt(document.getElementById('idcondicaoInicial').value, 10);
-
-//     console.log(resistencia);
-//     console.log(capacitancia);
-//     console.log(condicaoInicial);
-
-//     // if (isNaN(resistencia) || isNaN(capacitancia) || isNaN(condicaoInicial)) {
-//     //     alert('Por favor, insira valores numéricos válidos em todos os campos.');
-//     //     return;
-//     // }
-
-//     let resultado = 0;
-
-//     switch (parseInt(selectedImage)) {
-//         case 1:
-//             resultado = calc_c1(resistencia, capacitancia, condicaoInicial);
-//             break;
-//         case 2:
-//             resultado = calc_c2(resistencia, capacitancia, condicaoInicial);
-//             break;
-//         case 3:
-//             resultado = calc_c3(resistencia, capacitancia, condicaoInicial);
-//             break;
-//         case 4:
-//             resultado = calc_c4(resistencia, capacitancia, condicaoInicial);
-//             break;
-//         case 5:
-//             resultado = calc_c5(resistencia, capacitancia, condicaoInicial);
-//             break;
-//         case 6:
-//             resultado = calc_c6(resistencia, capacitancia, condicaoInicial);
-//             break;
-//         default:
-//     }
-
-//     // document.getElementById('chart').innerText = `Gráfico do Circuito ${selectedImage}`;
-//     document.getElementById('resultado').innerText = `Valor de Saída (V): ${resultado}V`;
-// }
-
-// function calc_c1(resistencia, capacitancia, condicaoInicial) {
-//     let somaTotal = resistencia + capacitancia + condicaoInicial;
-//     return somaTotal;
-// }
-
-// function calc_c2(resistencia, capacitancia, condicaoInicial) {
-//     let valorR = resistencia;
-//     return valorR;
-// }
-
-// function calc_c3(resistencia, capacitancia, condicaoInicial) {
-//     let valorC = capacitancia;
-//     return valorC;
-// }
-
-// function calc_c4(resistencia, capacitancia, condicaoInicial) {
-//     let valorCI = condicaoInicial;
-//     return valorCI;
-// }
-
-// function calc_c5(resistencia, capacitancia, condicaoInicial) {
-//     let somaRC = resistencia + capacitancia;
-//     return somaRC;
-// }
-
-// function calc_c6(resistencia, capacitancia, condicaoInicial) {
-//     let somaRCI = (resistencia + condicaoInicial);
-//     return somaRCI;
-// }
-
-// // Button script
-
-// let button = document.getElementById("button");
-
-// button.addEventListener('mousemove', (e) => {
-//     x = e.offsetX;
-//     y = e.offsetY;
-//     button.style.setProperty('--mouse-x', x + "px");
-//     button.style.setProperty('--mouse-y', y + "px");
-// });
-
 function validateAndSimulate() {
     var resistenciaInput = document.getElementById("resistanceInput");
     var capacitanciaInput = document.getElementById("capacitanceInput");
@@ -124,13 +41,9 @@ function validateAndSimulate() {
     }
 }
 
-
-
 // Testanfo os Gráficos
 
-// Adiciona um ouvinte de evento para o botão de simulação no HTML
 function simulate() {
-    // document.getElementById('simulateButton').addEventListener('click', function () {s
     const resistencia = parseFloat(document.getElementById('resistanceInput').value);
     const capacitancia = parseFloat(document.getElementById('capacitanceInput').value);
     const voltagem = parseFloat(document.getElementById('voltageInput').value);
